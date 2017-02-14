2d0acdc33daf1b94bba54b1f2a7ac4e8cef8cba9
hjl <1617451312@qq.oom>
2017-02-14 10:20:12 +0800

    搭建框架

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$("#pagepiling").pagepiling({
+	loopBottom:!0,
+	direction:'horizontal',
+	verticalCentered:true,
+	loopBottom:false,
+	loopTop:false,
+	sectionsColor:['#e78170','#22c3aa','#e78170','#22c3aa','#e78170'],
+	navigation:{position:"right",tooltips:['主页','教育背景','项目经验','擅长技术','联系我']},
+	onLeave:function(i,o,e){
+		
+	}});
